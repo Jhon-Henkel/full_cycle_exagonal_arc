c58035a8ca06390aa2babd82566c8dd18fe33498
jhowrf <jhowrf@gmail.com>
2024-08-23 07:17:27 -0300

    feat: Trabalhando com gomock e mockgen

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,24 @@
-package application
+package application_test
+
+import (
+	"github.com/Jhon-Henkel/full_cycle_hexagonal_arc/application"
+	mockapp "github.com/Jhon-Henkel/full_cycle_hexagonal_arc/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mockapp.NewMockIProduct(ctrl)
+	persistence := mockapp.NewMockIProductReader(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{Persistence: application.IProductPersistent{persistence, nil}}
+
+	result, err := service.Get("1")
+	assert.Nil(t, err)
+	assert.Equal(t, product, result)
+}
