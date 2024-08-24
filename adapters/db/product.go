fe50da764409a39fd6fc8406211616f74d2ec637
jhowrf <jhowrf@gmail.com>
2024-08-24 18:50:48 -0300

    feat: Implementando método Save

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -16,7 +16,7 @@ func NewProductDB(db *sql.DB) *ProductDB {
 
 func (p *ProductDB) Get(id string) (application.IProduct, error) {
 	var product application.Product
-	stmt, err := p.db.Prepare("select id, name, price, status from products where id = ?")
+	stmt, err := p.db.Prepare(`SELECT id, name, price, status FROM products WHERE id = ?`)
 	if err != nil {
 		return nil, err
 	}
@@ -25,4 +25,40 @@ func (p *ProductDB) Get(id string) (application.IProduct, error) {
 		return nil, err
 	}
 	return &product, nil
+}
+
+func (p *ProductDB) Save(product application.IProduct) (application.IProduct, error) {
+	var rows int
+	p.db.QueryRow(`SELECT id FROM products WHERE id = ?`, product.GetID()).Scan(&rows)
+	if rows == 0 {
+		return p.create(product)
+	}
+	return p.update(product)
+}
+
+func (p *ProductDB) create(product application.IProduct) (application.IProduct, error) {
+	stmt, err := p.db.Prepare(`INSERT INTO products (id, name, price, status) VALUES (?, ?, ?, ?)`)
+	defer stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductDB) update(product application.IProduct) (application.IProduct, error) {
+	_, err := p.db.Exec(
+		`UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?`,
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetID(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
