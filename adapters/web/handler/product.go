f9e7742152f637a3c11d18e6b81ba368f4bab540
jhowrf <jhowrf@gmail.com>
2024-08-27 07:20:33 -0300

    feat: Iniciando implementação dos handlers

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"github.com/Jhon-Henkel/full_cycle_hexagonal_arc/application"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"net/http"
+)
+
+func MakeProductHandlers(router *mux.Router, middleware *negroni.Negroni, service application.IProductService) {
+	router.Handle("/product/{id}", middleware.With(
+		negroni.Wrap(getProduct(service))),
+	).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service application.IProductService) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+	})
+}
