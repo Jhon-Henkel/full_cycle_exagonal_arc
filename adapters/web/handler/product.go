40da9708a36965a3b57f231674fac76393b904a7
jhowrf <jhowrf@gmail.com>
2024-08-28 07:11:35 -0300

    feat: Criando handler Create

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"github.com/Jhon-Henkel/full_cycle_hexagonal_arc/adapters/dto"
 	"github.com/Jhon-Henkel/full_cycle_hexagonal_arc/application"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
@@ -12,6 +13,9 @@ func MakeProductHandlers(router *mux.Router, middleware *negroni.Negroni, servic
 	router.Handle("/product/{id}", middleware.With(
 		negroni.Wrap(getProduct(service))),
 	).Methods("GET", "OPTIONS")
+	router.Handle("/product", middleware.With(
+		negroni.Wrap(createProduct(service))),
+	).Methods("POST", "OPTIONS")
 }
 
 func getProduct(service application.IProductService) http.Handler {
@@ -27,6 +31,31 @@ func getProduct(service application.IProductService) http.Handler {
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func createProduct(service application.IProductService) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var productDTO dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDTO)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+		product, err := service.Create(productDTO.Name, productDTO.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(JsonError(err.Error()))
 			return
 		}
 	})
